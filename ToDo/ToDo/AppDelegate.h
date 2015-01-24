9c1b0edd41b1d1ca9cbd4f8bb2cce145c132b497
monkeyde17 <monkey_tv@126.com>
2015-01-24 12:04:34 +0800

    create the project & start coding

---
+++ ToDo/ToDo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDo
+//
+//  Created by etond on 15/1/24.
+//  Copyright (c) 2015年 etond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDo/ToDo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDo
+//
+//  Created by etond on 15/1/24.
+//  Copyright (c) 2015年 etond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
